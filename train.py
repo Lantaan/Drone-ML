7e33fff57d4476ded142a1f2944a7617b490f710
Lantaan <69048952+Lantaan@users.noreply.github.com>
2025-01-20 20:07:22 +0100

    fix training

--- train.py
+++ train.py
@@ -1,19 +1,7 @@
 from stable_baselines3 import PPO
-import gym
+from environment.setup_env import setup_env
 
-from drone_2d_env import *
-from gym.envs.registration import register
-
-register(
-    id='drone-2d-custom-v0',
-    entry_point='drone_2d_env:Drone2dEnv',
-    kwargs={'render_sim': False, 'render_path': True, 'render_shade': True,
-            'shade_distance': 75, 'n_steps': 500, 'n_fall_steps': 10, 'change_target': False,
-            'initial_throw': False}
-)
-
-env = gym.make('drone-2d-custom-v0', render_sim=False, render_path=True, render_shade=True,
-            shade_distance=70, n_steps=500, n_fall_steps=10, change_target=True, initial_throw=True)
+env = setup_env(False)
 
 model = PPO("MlpPolicy", env, verbose=1)
 
