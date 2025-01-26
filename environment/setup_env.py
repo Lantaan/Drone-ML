65c53bd8839f8e8eaf7dba76a3b74a2f9ddc80a4
Zotelkopf <nicolasflaemig@web.de>
2025-01-26 03:12:58 +0100

    Add plot generation

--- environment/setup_env.py
+++ environment/setup_env.py
@@ -5,4 +5,4 @@ from config import wind_intensity
 def setup_env(render_sim, initial_throw):
     return gym.make('drone-2d-custom-v0', render_sim=render_sim, render_path=True, render_shade=True,
                     shade_distance=70, n_steps=500, n_fall_steps=10, change_target=True, 
-                    initial_throw=initial_throw, wind_intensity=wind_intensity)
+                    initial_throw=initial_throw, wind_intensity=wind_intensity, render_wind=True)
